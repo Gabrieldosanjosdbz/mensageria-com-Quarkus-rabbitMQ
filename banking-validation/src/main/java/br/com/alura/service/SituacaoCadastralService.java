18c3897a2045889d4c16d63601db5d86b23f8d8b
Gabriel dos Anjos <gabrieldosanjosdbz@gmail.com>
2026-09-18 20:34:40 -0300

    feat(banking-validation): criando agencia avro para serilização das mensagens

--- banking-validation/src/main/java/br/com/alura/service/SituacaoCadastralService.java
+++ banking-validation/src/main/java/br/com/alura/service/SituacaoCadastralService.java
@@ -3,7 +3,6 @@ package br.com.alura.service;
 import br.com.alura.domain.Agencia;
 import br.com.alura.domain.audit.Audit;
 import br.com.alura.repository.SituacaoCadastralRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.MutinyEmitter;
@@ -18,18 +17,16 @@ public class SituacaoCadastralService {
 
     private final Emitter<Audit> emitter;
 
-    private final MutinyEmitter<String> mutinyEmitter;
-
-    private final ObjectMapper objectMapper;
+    // nome qualificado porque br.com.alura.domain.Agencia já ocupa o nome simples
+    private final MutinyEmitter<br.com.alura.avro.Agencia> mutinyEmitter;
 
     public SituacaoCadastralService(SituacaoCadastralRepository situacaoCadastralRepository,
                                     @Channel("notificacoes") Emitter<Audit> emmiter,
-                                    @Channel("remover-agencia-channel") MutinyEmitter<String> mutinyEmitter
+                                    @Channel("remover-agencia-channel") MutinyEmitter<br.com.alura.avro.Agencia> mutinyEmitter
     ) {
         this.situacaoCadastralRepository = situacaoCadastralRepository;
         this.emitter = emmiter;
         this.mutinyEmitter = mutinyEmitter;
-        this.objectMapper = new ObjectMapper();
     }
 
     @WithTransaction
@@ -41,16 +38,20 @@ public class SituacaoCadastralService {
                     emitter.send(new Audit(agencia.getId(), agencia.getCnpj(), agencia.getSituacaoCadastral()));
                 })
                 .call(() -> {
-                    try {
-                         if(agencia.getSituacaoCadastral().equals("INATIVO")){
-                             String agenciaConvertida = objectMapper.writeValueAsString(agencia);
-                             return mutinyEmitter.send(agenciaConvertida);
-                         }
-                         return Uni.createFrom().voidItem();
-                    } catch (Exception e) {
-                        return Uni.createFrom().failure(e);
+                    if (agencia.getSituacaoCadastral().equals("INATIVO")) {
+                        return mutinyEmitter.send(paraAvro(agencia));
                     }
+                    return Uni.createFrom().voidItem();
                 })
                 .replaceWithVoid();
+    }
+
+    private br.com.alura.avro.Agencia paraAvro(Agencia agencia) {
+        return br.com.alura.avro.Agencia.newBuilder()
+                .setNome(agencia.getNome())
+                .setRazaoSocial(agencia.getRazaoSocial())
+                .setCnpj(agencia.getCnpj())
+                .setSituacaoCadastral(agencia.getSituacaoCadastral())
+                .build();
     }
 }
